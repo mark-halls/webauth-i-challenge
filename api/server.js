9bf47ae9b299ea1a6ae95be23bd77de3eab90366
Mark Halls <mark.halls@gmail.com>
2019-12-16 21:37:34 -0700

    Add db model and router

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require(`express`);
+
+const router = require(`../data/users/users-router`);
+
+const port = process.env.PORT || 4000;
+
+const server = express();
+
+server.use(`/api`, router);
+
+const start = () =>
+  server.listen(port, () =>
+    console.log(`\n **Server running on port ${port}**\n`)
+  );
+
+module.exports = { start };
